6e4c7c64f14ab40670a93c5d5a9d4e7b3c99c4ad
Andris Reinman <andris@kreata.ee>
2017-06-18 17:40:08 +0300

    update

--- dist.sh
+++ dist.sh
@@ -5,6 +5,6 @@ rm -rf npm-debug.log*
 rm -rf .git
 
 npm install --no-optional --production
-tar czf ../pangalink.tar.gz .
+tar czf ../pangalink-`jq -r .version package.json`.tar.gz .
 
 echo "done"
