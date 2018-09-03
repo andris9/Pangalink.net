eee1dbe40bbb2054dad22804da6503d1bda2b03d
Andris Reinman <andris@kreata.ee>
2018-09-03 12:48:32 +0300

    update

--- dist.sh
+++ dist.sh
@@ -3,8 +3,9 @@
 rm -rf node_modules
 rm -rf npm-debug.log*
 # rm -rf .git
+find . -name '.DS_Store' -type f -delete
 
-npm install --no-optional --production
+npm install --no-optional --production --no-bin-links --ignore-scripts
 tar czf ../pangalink-`jq -r .version package.json`.tar.gz --exclude '.git' --exclude '.gitignore' --exclude 'dist.sh' .
 
 echo "done"
